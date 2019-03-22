150a69c40e4dbefc8b7d9b833142b2681738c4f4
Hansel He <hansel@163.com>
2019-03-22 19:00:51 +0800

    support loop

--- src/pyWOL.py
+++ src/pyWOL.py
@@ -11,12 +11,21 @@
 import sys
 from optparse import OptionParser
 import socket
+import time
+import signal
+
+BROADCAST_ADDR = '<broadcast>'
+count = 0
 
 def define_cmd_line():
     usage = "usage: %prog [options] MAC"
     parser = OptionParser(usage)
     parser.add_option("-m", "--mac", dest="mac",
                       help="destination MAC address in format of xx:xx:xx:xx:xx:xx or xx-xx-xx-xx-xx-xx")
+    parser.add_option("-n", "--num", type="int", dest="num", default=1
+        , help="number of WOL packets to send")
+    parser.add_option("-t", "", action="store_true", default=False, dest="always"
+        , help="sent WOL packets until stopped")
     return parser
 
 def convert_mac_address(mac):
@@ -39,33 +48,40 @@ def send_wol_packet(data):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.bind(('', 0))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        num = s.sendto(data, ('<broadcast>', 9))
+        num = s.sendto(data, (BROADCAST_ADDR, 9))
     finally:
         s.close()
     return num
 
+def terminal(signum, frame):
+    print("\nTotal sent %d packets." % (count))
+    sys.exit(0)
+
 def main():
+    global count
     if len(sys.argv) < 2:
         print("no MAC address")
-        exit(-1)
+        sys.exit(-1)
 
     parser = define_cmd_line()
     (options, args) = parser.parse_args()
-    if options.mac:
-        mac = options.mac
-    else:
-        mac = sys.argv[1]
+    mac = options.mac if options.mac else args[0]
 
     try:
         mac_array = convert_mac_address(mac)
         data = build_magic_packet(mac_array)
-        num = send_wol_packet(data)
+        mac_str = ":".join(["%02X" % x for x in mac_array])
+        signal.signal(signal.SIGINT, terminal)
+        while count < options.num or options.always:
+            num = send_wol_packet(data)
+            print("#%d: sent %d bytes to MAC=%s" % (count, num, mac_str))
+            count = count + 1
+            time.sleep(1)
     except ValueError as e:
         print("Error: " + e.args[0])
-        exit(-2)
+        sys.exit(-2)
 
-    mac_str = ":".join(["%02X"%x for x in mac_array])
-    print("Sent %d bytes to MAC: %s" % (num, mac_str))
+    print("\nTotal sent %d packets." % (count))
 if __name__ == '__main__':
     main()
 
