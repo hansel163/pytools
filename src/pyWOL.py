09afe4807a5af4eb560859ec2b875bf68aea87be
Hansel He <hansel@163.com>
2019-04-02 18:05:23 +0800

    support customized IP and port

--- src/pyWOL.py
+++ src/pyWOL.py
@@ -24,9 +24,11 @@ def define_cmd_line():
     parser.add_option("-m", "--mac", dest="mac",
                       help="destination MAC address in format of xx:xx:xx:xx:xx:xx or xx-xx-xx-xx-xx-xx")
     parser.add_option("-n", "--num", type="int", dest="num", default=1
-        , help="number of WOL packets to send")
+        , help="number of WOL packets to send, default is 1")
     parser.add_option("-t", "", action="store_true", default=False, dest="always"
         , help="sent WOL packets until stopped")
+    parser.add_option("-a", "--address", dest="ip", default=BROADCAST_ADDR, help="Destination IP address, default is 255.255.255.255")
+    parser.add_option("-p", "--port", type=int, dest="port", default=UDP_PORT, help="Destination UDP port, default is 9")
     return parser
 
 def convert_mac_address(mac):
@@ -43,13 +45,13 @@ def build_magic_packet(mac_array):
     data_str = 'FF'*6 + mac_data*16
     return bytes().fromhex(data_str)
 
-def send_wol_packet(data):
+def send_wol_packet(data, ip, port):
     num = 0
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.bind(('', 0))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        num = s.sendto(data, (BROADCAST_ADDR, UDP_PORT))
+        num = s.sendto(data, (ip, port))
     finally:
         s.close()
     return num
@@ -67,6 +69,8 @@ def main():
     parser = define_cmd_line()
     (options, args) = parser.parse_args()
     mac = options.mac if options.mac else args[0]
+    ip = options.ip if options.ip else BROADCAST_ADDR
+    port = options.port if options.port else UDP_PORT
 
     try:
         mac_array = convert_mac_address(mac)
@@ -74,9 +78,9 @@ def main():
         mac_str = ":".join(["%02X" % x for x in mac_array])
         signal.signal(signal.SIGINT, terminal)
         while count < options.num or options.always:
-            num = send_wol_packet(data)
+            num = send_wol_packet(data, ip, port)
             print("#%d: sent %d bytes WOL packet to ip=%s:%d for MAC=%s"
-                % (count, num, BROADCAST_ADDR, UDP_PORT, mac_str))
+                % (count, num, ip, port, mac_str))
             count = count + 1
             time.sleep(1)
     except ValueError as e:
